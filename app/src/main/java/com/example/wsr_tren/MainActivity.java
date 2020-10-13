5cb52f2ae338391730764d16f80a7507a77412ad
Waret <shihalev0187@mail.ru>
2020-10-13 23:02:47 +0400

    add activity user

--- app/src/main/java/com/example/wsr_tren/MainActivity.java
+++ app/src/main/java/com/example/wsr_tren/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
        if (actionBar != null){
            actionBar.hide();
        }
-       textViewTest = findViewById(R.id.text);
+       textViewTest = findViewById(R.id.textViewTest);
     }
 
     public void onClickOtdBank(View view) {
@@ -37,35 +37,36 @@ public class MainActivity extends AppCompatActivity {
     public void onClickKurs(View view) {
         Intent intent = new Intent(this, KursActivity.class);
         startActivity(intent);
-
-    //диалоговое окно
-    private void  myCustomDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        ConstraintLayout cl = (ConstraintLayout) getLayoutInflater().inflate(R.layout.my_dialog,null);
-        builder.setView(cl);
-        final Intent intent = new Intent(this, AccountActivity.class);
-
-        builder.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            AlertDialog alertDialog = (AlertDialog) dialog;
-                EditText editText = alertDialog.findViewById(R.id.editTextTextPersonLogin);
-                assert editText != null;
-                textViewTest.setText(editText.getText().toString());
-                alertDialog.dismiss();
-                startActivity(intent);
-            }
-        });
-        builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-
-            }
-        });
-        AlertDialog dialog = builder.create();
-        dialog.show();
     }
+        //диалоговое окно
+        private void myCustomDialog () {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            ConstraintLayout cl = (ConstraintLayout) getLayoutInflater().inflate(R.layout.my_dialog, null);
+            builder.setView(cl);
+
+
+            builder.setPositiveButton("Войти", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                    AlertDialog alertDialog = (AlertDialog) dialog;
+                    EditText editText = alertDialog.findViewById(R.id.editTextTextPersonLogin);
+                    assert editText != null;
+                    textViewTest.setText(editText.getText().toString());
+                    alertDialog.dismiss();
+                    Intent intent = new Intent(getApplicationContext(), AccountActivity.class);
+                    startActivity(intent);
+                }
+            });
+            builder.setNegativeButton("Отмена", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
 
 
 
