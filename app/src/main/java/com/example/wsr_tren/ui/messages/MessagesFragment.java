b519509b860653264cf10ee45d7fe08cc48281b0
Waret <shihalev0187@mail.ru>
2020-10-18 15:14:36 +0400

    log layout

---
+++ app/src/main/java/com/example/wsr_tren/ui/messages/MessagesFragment.java
@@ -0,0 +1,36 @@
+package com.example.wsr_tren.ui.messages;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.wsr_tren.R;
+import com.example.wsr_tren.ui.home.HomeViewModel;
+
+public class MessagesFragment extends Fragment {
+
+    private MessagesViewModel messagesViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        messagesViewModel =
+                ViewModelProviders.of(this).get(MessagesViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_messages, container, false);
+        final TextView textView = root.findViewById(R.id.text_message);
+        messagesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
